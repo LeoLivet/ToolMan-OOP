951363ae81ae3d28b50c45d62200a8435698ba93
LeoLivet <CharlyLLarsen@gmail.com>
2025-02-21 14:29:26 +0100

    Change Playercontrols added gravity to charactercontroller

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -7,14 +7,18 @@ public class PlayerControls : MonoBehaviour
     private InputAction _jumpAction;
     [SerializeField] CharacterController controller;
     [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float jumpForce = 10f;
-    private Rigidbody _rigidbody;
+    [SerializeField] private float jumpHeight = 3f;
     
-
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public float gravity = -9.81f;
+    Vector3 velocity;
+    bool isGrounded;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _rigidbody = GetComponent<Rigidbody>();
         _moveAction = InputSystem.actions.FindAction("Move");
         _jumpAction = InputSystem.actions.FindAction("Jump");
     }
@@ -22,18 +26,25 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        
         Vector3 moveValue = _moveAction.ReadValue<Vector2>();
         
-        if(_moveAction.IsPressed())
+        Vector3 move = transform.right * moveValue.x + transform.forward * moveValue.y;
+        controller.Move(move * (moveSpeed * Time.deltaTime));
+        
+        if (_jumpAction.IsPressed() && isGrounded)
         {
-            transform.Translate(new Vector3(moveValue.x, 0, moveValue.y) * (moveSpeed * Time.deltaTime));
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
         
-        if (_jumpAction.IsPressed())
-        {
-            _rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            
-        }
+        velocity.y += gravity * Time.deltaTime;
         
+        controller.Move(velocity * Time.deltaTime);
     }
 }
